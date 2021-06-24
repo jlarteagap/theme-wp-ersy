67b87c7c278664a1b389fbb3425f52e2efa9bc5e
Jorge <jorgelartearteagap@gmail.com>
2021-06-24 14:29:03 -0400

    first post, with grid layout

--- index.php
+++ index.php
@@ -15,48 +15,44 @@
 get_header();
 ?>
 
-	<main id="primary" class="container">
+	<main id="primary" class="container my-5">
+
 		<?php
 		if ( have_posts() ) :
 			$first_post = true;
+			$the_query = new WP_Query( 'posts_per_page=5' );
+
 			if ( is_home() && ! is_front_page() ) :
 				?>
 				<header>
 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
 				</header>
 				<?php
-			endif;
-			
-			?>
-				<div class="featured__section d-flex align-items-start">
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) :
-					the_post();
-					/*
-					* Include the Post-Type-specific template for the content.
-					* If you want to override this in a child theme, then include a file
-					* called content-___.php (where ___ is the Post Type name) and that will be used instead.
-					*/
-					// get_template_part( 'template-parts/content', get_post_type() );
+			endif;?>
 
+			 <div class="featured__posts">
+				<?php 
+					while ($the_query -> have_posts()) : $the_query -> the_post(); 
+					
 					if ($first_post){ 
 						$first_post = false; 
 						?>
-						<div class="first__post col-12 col-md-6 p-0 pe-md-3">
-							<!-- first post thumbnail  -->
-							<?php ersy_firts_post_thumbnail('') ?>
-							<div class="post__card-category">
-								<!-- firts post category  -->
-								<?php the_category( ' ' ); ?>
+						<div class="featured__posts--top">
+							<div class="first__post col-12 p-0 pe-md-3">
+								<!-- first post thumbnail  -->
+								<?php ersy_firts_post_thumbnail('') ?>
+								<div class="post__card-category">
+									<!-- firts post category  -->
+									<?php the_category( ' ' ); ?>
+								</div>
+								<!-- first post title  -->
+								<?php
+									the_title( '<h2 class="post__card--title">
+										<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+								?>
 							</div>
-							<!-- first post title  -->
-							<?php
-								the_title( '<h2 class="post__card--title">
-									<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-							?>
 						</div>	
-					<?php } else { ?>
+						<?php } else { ?>
 							<!-- <div class="row gx-3 gy-2"> -->
 								<div class="col-6 col-md-12">
 									<div class="post__card d-block d-md-inline-flex">
@@ -75,15 +71,14 @@ get_header();
 									</div>
 								</div>
 							<!-- </div> -->
+
 					<?php }
-						endwhile; ?>
-				</div>
+					// Repeat the process and reset once it hits the limit
+					endwhile;
+					wp_reset_postdata();
+					?>
 			</div>
-			<?php 
-			the_posts_navigation();
-
-		else :
-
+		<?php else :
 			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
@@ -92,5 +87,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
